451dee45c5a81bfbafac26ace8ff4e2947dce460
KaranjaK <karanjakelvin77@gmail.com>
2026-05-05 13:23:13 +0300

    It will create section IDs for scrolling aspects

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { Navbar } from './components/Navbar'
 
+const SECTION_IDS = ["home", "about", "skills", "experience", "projects", "contact"];
+
 export default function App() {
   return (
     <>
